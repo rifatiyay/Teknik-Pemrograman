d1879c13fdbfca66c59ac139ce9b2d3a48254174
Rifatia Yumna Salma <76715783+rifatiyay@users.noreply.github.com>
2021-05-01 10:09:36 +0700

    Add files via upload

---
+++ Topic 7/src/Lat7_2/MethodOverloading.java
@@ -0,0 +1,16 @@
+package Lat7_2;
+
+public class MethodOverloading {
+	
+	public static void main (String args[]){
+		
+		Overload obj = new Overload();
+		double result;
+		
+		obj.demo(10);
+		obj.demo(10, 20);
+		result = obj.demo(5.5);
+		
+		System.out.println("O/P : " + result);
+	}
+}
---
+++ Topic 7/src/Lat7_3/Firm.java
@@ -0,0 +1,10 @@
+package Lat7_3;
+
+public class Firm {
+	
+	public static void main(String args[]){
+		
+		Staff personnel = new Staff();
+		personnel.payday();
+	}
+}
