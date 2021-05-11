b02ed22ba0725dcd0e2e2292dde293446982adee
Rifatia Yumna Salma <76715783+rifatiyay@users.noreply.github.com>
2021-05-11 14:00:04 +0700

    Add files via upload

---
+++ Week 8/TugasPLOO_ATM/src/ATM/ATMMain.java
@@ -0,0 +1,9 @@
+package ATM;
+
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main (String[] args){
+	   ATM theATM = new ATM();
+	   theATM.run();
+   }
+}
